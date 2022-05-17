5513246ab33f919772e8d0853183d75641134f08
a-klimovich <ar2r.klimovich@gmail.com>
2022-05-17 20:34:46 +0200

    base

---
+++ public/js/main.js
@@ -0,0 +1,11 @@
+"use strict";
+
+(function () {
+  console.log('What is the date: XYI');
+
+  var a = function a(_a, b) {
+    return Math.pow(_a, b);
+  };
+
+  a(2, 13);
+})();
---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+(function() {
+  console.log('What is the date: XYI');
+
+  const a = (a, b) => a**b;
+  
+  a(2, 13);
+})()
